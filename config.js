fd813178fc1298f4b42ed828119d9cdae4fb9216
Whitevinyl <neverest_songs@yahoo.co.uk>
2016-09-25 19:07:16 +0100

    procfile and cairo/node build

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    twitter: {
+        consumer_key:         '9xfUDolc2CLxX3iPoBu8cU182',
+        consumer_secret:      'EFaH9TmeEuAJ7RNxR9AfkVaaIjOqPDzZvdH4y9L4ALEDfU6jMI',
+        access_token:         '771413200210391040-SwZ18V1reo2zp4BEhOSVa72vzKya29o',
+        access_token_secret:  'ruIoubhsSQgWqPpnOhFe3KTggqcxM6H2EhwbpyzhhwrpV',
+        timeout_ms:           60*1000  // optional HTTP request timeout to apply to all requests.
+    },
+    soundcloud: {
+        client_id : '93e521afd40a15d1d6c39da1c4dba29f',
+        client_secret : '8a8cb063fe8bf1562b849307fd62983c',
+        username : 'arpobservatory@gmail.com',
+        password : 'FilterSubSwell'
+    }
+};
